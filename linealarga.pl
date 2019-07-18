94467475cf1d3aee85e1c381e6aa9c92bfa7ff18
jomaldon <13171084+jomaldon@users.noreply.github.com>
2019-07-18 16:14:43 -0400

    nuevos archivos

---
+++ linealarga.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+# linealarga.pl
+# Nicolas Loira
+# ultima version: mayo 2005
+# (corregido bug de primera linea)
+# (en caso de no dar archivo de salida, arroja al stdout)
+
+# Transforma un archivo de secuencias en formato fasta con ancho maximo de columna
+# en uno con una fila por secuencia. Util cuando se desea buscar patrones.
+
+# Uso: 
+# linealarga.pl fasta.seq fasta.linealarga.seq
+# o tambien:
+# linealarga.pl fasta.seq > fasta.linealarga.seq
+
+open INFILE, "$ARGV[0]";
+
+if($#ARGV==0){
+	open OUTFILE, ">&STDOUT";
+}else{
+	open OUTFILE, ">$ARGV[1]";
+}
+
+$buffer = "";
+
+while (<INFILE>){
+
+	if( />/ ){
+		$buffer =~ tr/atgcn/ATGCN/;
+		if($buffer ne ""){
+			print OUTFILE $buffer."\n".$_;
+		}else{
+			print OUTFILE $_;
+		}
+
+		$buffer ="";
+	}
+	else{
+		chomp($_);
+		$buffer .= $_;
+	}
+}
+
+$buffer =~ tr/atgcn/ATGCN/;
+print OUTFILE $buffer."\n";
+
+close INFILE;
+close OUTFILE;
+
---
+++ memlog.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+## Script de bash (utuntu 16.04) para monitorear estado de la RAM
+
+echo "      date     time $(free -m | grep total | sed -E 's/^    (.*)/\1/g')"
+while true; do
+    echo "$(date '+%Y-%m-%d %H:%M:%S') $(free -m | grep Mem: | sed 's/Mem://g')"
+    sleep 5
+done
