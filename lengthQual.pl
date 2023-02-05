6fb00c7b93b53850c699a9c3fd6301fe1d58ae49
Jonathan Maldonado <13171084+jomaldon@users.noreply.github.com>
2023-02-04 22:11:32 -0300

    Create lengthQual.pl

---
+++ lengthQual.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use Bio::SeqIO;
+
+my $input_file = shift;
+
+my $seq_in  = Bio::SeqIO->new( -format => 'qual',
+                                   -file => $input_file);
+
+my $seq;
+# my $total=0;
+# my $n=0;
+
+while( $seq = $seq_in->next_seq() ) {
+#  $total+=$seq->length; $n++;
+  print $seq->id();
+  print "\t";
+  print $seq->length;
+  print "\n";
+}
+
+#print ($total/$n);
+#print "\n";
