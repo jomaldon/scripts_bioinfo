1c176a2849ad3da5ed2e1e435a3ec7cbd6105d64
Jonathan Maldonado <13171084+jomaldon@users.noreply.github.com>
2023-06-24 21:28:47 -0400

    Update fastq-to-avg_qual.py

--- fastq-to-avg_qual.py
+++ fastq-to-avg_qual.py
@@ -1,3 +1,12 @@
+#! /usr/bin/env python
+#
+# @author: Jonathan Maldonado <jomaldon at gmail dot com>
+# @update: Jun 24 2023
+#
+# Biopython required:
+# "pip install biopython" or "sudo apt update; sudo apt install biopython" 
+#
+
 from Bio import SeqIO
 import gzip
 import sys
